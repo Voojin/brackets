2082a201cf9bf2a871f7f2f4333933d7e961793d
Vyacheskav Naryshkin <voojin@bk.ru>
2022-02-24 22:04:49 +0800

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) return false;
+  if (str.length === 0) return true;
+
+  let pairsStack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let last = pairsStack[pairsStack.length - 1];
+
+    for (let [open, closed] of bracketsConfig) {
+      if (
+        (str[i] === open && open !== closed) ||
+        (str[i] === open && open === closed && last !== str[i])
+      ) {
+        pairsStack.push(str[i]);
+      } 
+      else if (str[i] === closed && last === open) {
+        pairsStack.pop();
+      }
+    }
+  }
+  return !pairsStack.length;
+};
